86a77c18321a4e85df1724f049a4bdc91f4cb222
Hajïn_ <firitusow@gmail.com>
2022-10-12 02:19:47 +0200

    Initial commit

---
+++ src/main/java/com/tubyapp/tuby/TubyApplication.java
@@ -0,0 +1,13 @@
+package com.tubyapp.tuby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TubyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tubyapp/tuby/TubyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubyapp.tuby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
